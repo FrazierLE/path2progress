94d043b83074f97b85d8f9e1d16eb01445fb1376
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-05-04 17:55:05 -0600

    Update heading

--- pages/goals.js
+++ pages/goals.js
@@ -1,12 +1,15 @@
 import Protein from "../components/goals/protein"
 import Water from "../components/goals/water"
 import Steps from "../components/goals/steps"
-import { Container, Box, Typography } from "@mui/system"
+import { Container, Box } from "@mui/system"
+import { Typography } from "@mui/material"
 
 export default function Goals() {
   return(
     <Container sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      <h1>Goals</h1>
+      <Typography variant='h1'>
+        Goals
+      </Typography>
       <Box 
         sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '80vh'}}
       >
